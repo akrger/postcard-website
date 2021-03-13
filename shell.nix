dd382c06f0ca43da0fc9bc8805b52cc8b7cb8afe
André Krüger <222393+akrger@users.noreply.github.com>
2021-03-13 17:16:16 +0100

    wip

--- shell.nix
+++ shell.nix
@@ -1,8 +1,27 @@
+# let
+# in { nixpkgs ? import <nixpkgs> { } }:
+# with nixpkgs;
+# mkShell { buildInputs = [ unstable.nodejs-15_x nixfmt rustup  ]; }
+
 let
-unstable = import
-    (builtins.fetchTarball https://github.com/nixos/nixpkgs/tarball/87645e0250aec3b64cace2858fbebc6265e7aa04)
-    # reuse the current configuration
-  {  };
-in { nixpkgs ? import <nixpkgs> { } }:
-with nixpkgs;
-mkShell { buildInputs = [ unstable.nodejs-15_x nixfmt rustup  ]; }
+  moz_overlay = import (builtins.fetchTarball
+    "https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz");
+  unstable = import (builtins.fetchTarball
+    "https://github.com/nixos/nixpkgs/tarball/87645e0250aec3b64cace2858fbebc6265e7aa04")
+    { };
+  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+  rust = (nixpkgs.rustChannelOf {
+    date = "2020-12-31";
+    channel = "stable";
+  }).rust.override { extensions = [ "rust-src" ]; };
+in with nixpkgs;
+stdenv.mkDerivation {
+  name = "moz_overlay_shell";
+  buildInputs = [
+    nixfmt
+    unstable.nodejs-15_x
+    rust
+    # to use a specific nighly:
+    # to use the project's rust-toolchain file:
+  ];
+}
